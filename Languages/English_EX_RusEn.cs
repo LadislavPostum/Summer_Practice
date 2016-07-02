669392a1ff064cb22432523c96bd9fab3a8af3a4
LadislavPostum <ladislavpostum@gmail.com>
2016-07-03 00:52:10 +0600

    Добавлено полноценное упражнение для английского языка. Но почему то уезжают строки. Теперь дело за малым.

--- Languages/English_EX_RusEn.cs
+++ Languages/English_EX_RusEn.cs
@@ -20,6 +20,7 @@ namespace Languages
         }
 
         int CloseFlag;
+        int i, j = 0;
         private English_Exercises _EE;
         public English_EX_RusEn(English_Exercises EE, int a, int b)
         {
@@ -39,6 +40,36 @@ namespace Languages
             toolTip1.SetToolTip(button1, "Назад");
             toolTip2.SetToolTip(button2, "Показать перевод");
             toolTip3.SetToolTip(button3, "Следующее слово");
+
+            this.tableTableAdapter.Fill(this.english_DataBaseDataSet.Table);
+
+            i = this.english_DataBaseDataSet.Table.Rows.Count;
+
+            String s1 = i.ToString();
+            label2.Text = s1;
+            
+            Random rand = new Random();
+            int k = 0;
+            if (i != 0)
+            {
+                j = rand.Next(i);
+                k = j + 1;
+            }            
+
+            String s2 = k.ToString();
+            label1.Text = s2;
+
+            if (i == 0)
+            {
+                this.label4.Text = "";
+                this.label5.Text = "";
+            }
+                        
+            if (i != 0)
+            {
+                label4.Text = this.english_DataBaseDataSet.Table.Rows[j][2].ToString();
+                label5.Text = this.english_DataBaseDataSet.Table.Rows[j][1].ToString();
+            }            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -60,6 +91,38 @@ namespace Languages
         private void button2_Click(object sender, EventArgs e)
         {
             this.label5.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.label5.Visible = false;
+
+            int l = j;
+            Random rand = new Random();
+            int k = 0;
+            if (i != 0)
+            {
+                while (l == j)
+                {
+                    j = rand.Next(i);
+                    k = j + 1;
+                }                
+            }
+
+            String s2 = k.ToString();
+            label1.Text = s2;
+
+            if (i == 0)
+            {
+                this.label4.Text = "";
+                this.label5.Text = "";
+            }
+
+            if (i != 0)
+            {
+                label4.Text = this.english_DataBaseDataSet.Table.Rows[j][2].ToString();
+                label5.Text = this.english_DataBaseDataSet.Table.Rows[j][1].ToString();
+            }            
         }
     }
 }
